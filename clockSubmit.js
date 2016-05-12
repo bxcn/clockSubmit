24ed7060ff3fe70ca1d531c1bde5386a4334db20
bxcn <hi_beijing@126.com>
2016-05-12 14:24:22 +0800

    V2.0.1
    解决锁定和未锁定逻辑判断

--- clockSubmit.js
+++ clockSubmit.js
@@ -6,11 +6,11 @@
   window.console = console || {log: function() {}};
   // 核心
   function Clocked() {
-    this._isOpen = true;//锁
+    this._clockStatus = true;//锁
     this.timer = null; // 定时器
   }
   Clocked.prototype.init = function() {
-    //this._isOpen = true;
+    //this._clockStatus = true;
   }
 
   // 返回 true:锁是开着的，可以提交表单；false:销是关阗的，不可以提交表单；
@@ -19,34 +19,42 @@
     var that = this;
     /*
       1、判断锁是开着的
-      2、把_isOpen开关给关闭false
-      3、添加定时器，定时器在1000毫秒内是_isOpen是关着的。1000毫秒后是再放开_isOpen
+      2、把_clockStatus开关给关闭false
+      3、添加定时器，定时器在1000毫秒内是_clockStatus是关着的。1000毫秒后是再放开_clockStatus
     */
-    if ( that._isOpen ) {
-      that._isOpen = false;
+    if ( that._clockStatus ) {
+      // 锁住
+      that.isClose(true);
+
       that.timer = window.setTimeout(function(){
         that.timer = null;
-        that._isOpen = true;
-      }, 1000);
+        // 打开锁
+        that.isOpen(true);
+      }, 3000);
 
       // 这里返回的true是说明这是第一次提交表单当然是可以提交的，所以会返回true.
-      // 连续点提交就不会再执行这里了，原因第一次进到这个语句块后，就改变了_isOpen的状态；
-      return true;
+      // 连续点提交就不会再执行这里了，原因第一次进到这个语句块后，就改变了_clockStatus的状态；
+      return false;
     }
 
     // 在定时器未销毁之前，返回的都是false;
-    return that._isOpen;
+    return that.isClose();
   }
 
-  // 返回 true:锁是开着的，可以提交表单；false:销是关阗的，不可以提交表单；
-  Clocked.prototype.isOpen = function() {
-
-    return this._isOpen;
+  // 返回 true:锁是开着的;
+  Clocked.prototype.isOpen = function( b ) {
+    if ( arguments.length == 1 ) {
+      this._clockStatus = b;
+    }
+    return this._clockStatus;
   }
 
-  // 返回 true:锁是开着的，可以提交表单；false:销是关阗的，不可以提交表单；
-  Clocked.prototype.isClose = function() {
-    return !this._isOpen;
+  // 返回 true:锁是关着的;
+  Clocked.prototype.isClose = function( b ) {
+    if ( arguments.length == 1 ) {
+      this._clockStatus = !b;
+    }
+    return !this._clockStatus;
   }
 
   var _clocked = null;
@@ -67,17 +75,18 @@
     // 单例模式共享一个实例对象
     var c = Clock.init();
 
-    if ( c.isClose() ) {
-      return function() {};
-    }
-
     return function( callback, param ) {
 
-      if ( c.clock() ) {
-        callback.call(this, param);
+      // false是没有锁定 true表示已经锁定不能提交了
+      var isClock = c.clock();
+
+      if ( isClock ) {
+        console.log("锁定");
       } else {
-        console.log("锁定")
+        console.log("解锁")
       }
+
+      return isClock;
     }
 
   }();
