230b1826141bd7ee7fe845167fb9a54b20da01b2
bxcn <hi_beijing@126.com>
2016-05-13 13:18:26 +0800

    V2.3.0

--- clockSubmit.js
+++ clockSubmit.js
@@ -2,18 +2,23 @@
   Open Source
   https://github.com/bxcn/clockSubmit
 */
-;(function( ) {
-  window.console = console || {log: function() {}};
+;
+(function() {
+  window.console = console || {
+    log: function() {}
+  };
   // 核心
   function Clocked() {
-    this._clockStatus = true;//锁
+    this._clockStatus = true; //锁
     this.timer = null; // 定时器
     this.grapTimer = 1000; // 锁定后，1秒钟后解锁
   }
-  Clocked.prototype.setTimer = function( grapTimer ) {
+  Clocked.prototype.init = function(grapTimer) {
+    this.setTimer( grapTimer );
+  }
+  Clocked.prototype.setTimer = function(grapTimer) {
     this.grapTimer = grapTimer || this.grapTimer;
   }
-
   // 返回 true:锁是开着的，可以提交表单；false:销是关阗的，不可以提交表单；
   Clocked.prototype.clock = function() {
 
@@ -23,12 +28,12 @@
       2、把_clockStatus开关给关闭false
       3、添加定时器，定时器在1000毫秒内是_clockStatus是关着的。1000毫秒后是再放开_clockStatus
     */
-    if ( that._clockStatus ) {
+    if (that._clockStatus) {
 
       // 锁住
       that.isClose(true);
 
-      that.timer = window.setTimeout(function(){
+      that.timer = window.setTimeout(function() {
         that.timer = null;
         // 打开锁
         that.isOpen(true);
@@ -44,60 +49,87 @@
   }
 
   // 返回 true:锁是开着的;
-  Clocked.prototype.isOpen = function( b ) {
-    if ( arguments.length == 1 ) {
+  Clocked.prototype.isOpen = function(b) {
+    if (arguments.length == 1) {
       this._clockStatus = b;
     }
     return this._clockStatus;
   }
 
   // 返回 true:锁是关着的;
-  Clocked.prototype.isClose = function( b ) {
-    if ( arguments.length == 1 ) {
+  Clocked.prototype.isClose = function(b) {
+    if (arguments.length == 1) {
       this._clockStatus = !b;
     }
     return !this._clockStatus;
   }
 
-  var _clocked = null;
+  // var _clocked = null;
 
-  // this is a Singleton Pattern
-  window.clockSubmit =  {
-    init: function( grapTimer ) {
-      if ( !_clocked ) {
+/*  // this is a Singleton Pattern
+  window.clock = {
+    init: function(grapTimer) {
+      if (!_clocked) {
         _clocked = new Clocked();
       }
 
-      _clocked.setTimer( grapTimer );
+      _clocked.setTimer(grapTimer);
 
       // false是没有锁定 true表示已经锁定不能提交了
       var isClock = _clocked.clock();
 
       return isClock;
     }
+  }*/
+
+  function clockSubmitProxy() {
+    this._clocked = null;
   }
 
-/*
-  // 扩展
-  window.clockSubmit = function () {
-    // 单例模式共享一个实例对象
-    var c = c = Clock.init();
-    return {
-      init: function( grapTimer ) {
-        Clock.setTimer( grapTimer );
-        // false是没有锁定 true表示已经锁定不能提交了
-        var isClock = c.clock();
-        return isClock;
-      },
-      isOpen: function( b ) {
-        return c.isOpen(b);
-      },
-      isClose: function( b ) {
-        return c.isClose(b);
-      }
-
+  clockSubmitProxy.prototype.init = function(grapTimer) {
+    if (!this._clocked) {
+      this._clocked = new Clocked();
     }
-  }();*/
+
+    this._clocked.init(grapTimer);
+
+    // false是没有锁定 true表示已经锁定不能提交了
+    var isClock = this._clocked.clock();
+
+    return isClock;
+  };
+  clockSubmitProxy.prototype.isOpen = function(b) {
+    return this._clocked.isOpen(b);
+  };
+  clockSubmitProxy.prototype.isClose = function(b) {
+    return this._clocked.isClose(b);
+  };
+
+  window.clockSubmit =  new clockSubmitProxy();
+
+
+
+  /*
+    // 扩展
+    window.clockSubmit = function () {
+      // 单例模式共享一个实例对象
+      var c = c = Clock.init();
+      return {
+        init: function( grapTimer ) {
+          Clock.setTimer( grapTimer );
+          // false是没有锁定 true表示已经锁定不能提交了
+          var isClock = c.clock();
+          return isClock;
+        },
+        isOpen: function( b ) {
+          return c.isOpen(b);
+        },
+        isClose: function( b ) {
+          return c.isClose(b);
+        }
+
+      }
+    }();*/
 
 })();
 
