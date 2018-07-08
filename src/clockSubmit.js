bb4d34a4b4e8ff471c4f1b8c072e6ae6553bee64
dengyanlu <dengyanlu@qiaodata.com>
2018-07-08 23:22:26 +0800

    add

--- src/clockSubmit.js
+++ src/clockSubmit.js
@@ -4,25 +4,27 @@ class _ClockSubmit {
   constructor(sign, grapTimer) {
     this.grapTimer = grapTimer || 1000;
     this.sign = sign;
+    this.signTimer = `${sign}timer`;
   }
 
   clock() {
     const sign = this.sign;
-    if (global[sign + 'timer']) {
+    const signTimer = this.signTimer;
+    if (global[signTimer]) {
       // 第一次返回false
       return true;
     }
     // 添加定时器，定时器在1000毫秒内是status是关着的。1000毫秒后是再放开status
-    global[sign + 'timer'] = window.setTimeout(() => {
+    global[signTimer] = window.setTimeout(() => {
       this.open();
     }, this.grapTimer);
     return false;
   }
   open() {
     const sign = this.sign;
-    global[sign] = null; // 释放引用
-    window.clearTimeout(global[sign + 'timer']);
-    global[sign + 'timer'] = null;
+    const signTimer = this.signTimer;
+    window.clearTimeout(global[signTimer]);
+    global[signTimer] = global[sign] = null; // 释放引用
   }
 }
 
